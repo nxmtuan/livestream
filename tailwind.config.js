db7a928fa7700bdb070ac2c5e9e205b6641ffaba
tuannguyen <coixaygio107@gmail.com>
2025-03-20 23:02:58 +0700

    Add match Card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{php,js,html}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        borderCardGradient:
+          "linear-gradient(to right, #F15E23, #FFB89C, #F15E23, #FFC2AA, #F15E23)",
+        timeBorderCardGradient: "linear-gradient(to right, #FC5D1D, #B03807)",
+        timeCardGradient: "linear-gradient(to right, #000000, #666666)",
+        rateGradient: "linear-gradient(to bottom, #454343CC, #696767CC)",
+        inMatchGradient: "linear-gradient(to right, #37B349, #FC9504)",
+        scoreGradient: "linear-gradient(to right, #000000CC, #666666CC)",
+      },
+    },
     container: {
       center: true,
       padding: "1rem",
